b5580aaa38b3c2331d06ed10734cc1cd41d0227e
Damaris <correofalso@gmail.com>
2017-12-15 20:50:19 +0100

    primera subida

---
+++ programacion_02/Assets/scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag ("Player");
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		float frontBack = Input.GetAxis ("Horizontal");
+		float leftRight = Input.GetAxis ("Vertical");
+
+		float rotationLeftRight = Input.GetAxis ("Mouse X");
+		float rotationUpDown = Input.GetAxis ("Mouse Y");
+
+		Vector3 v = new Vector3 (frontBack, 0.0f, leftRight);
+		player.GetComponent<Rigidbody> ().AddForce (v);
+
+		Quaternion localRotation = transform.rotation * (Quaternion.AngleAxis (100 * rotationLeftRight * Time.deltaTime * Vector3.up) *
+		                           Quaternion.AngleAxis (100 * rotationUpDown * Time.deltaTime, Vector3.left));
+		
+		transform.rotation = localRotation;
+
+		
+	}
+}
